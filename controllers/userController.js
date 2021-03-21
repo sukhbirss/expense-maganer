225451b3892e52c445f4e1d9710678c3e4ebf83f
sukhbirss <s.sukhbirsohal@gmail.com>
2021-03-21 06:57:36 +0530

    garbage data removed

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,25 +11,7 @@ exports.getme = catchAsync(async(req,res,next) => {
     user
   })
 });
-// });
-// exports.getAllUser = catchAsync(async(req,res,next) => {
-//   user = await User.find();
-//   res.status(200).json({
-//     status:'success',
-//     user
-//   })
 
-// });
-
-// exports.getUserData = catchAsync(async(req,res)=>{
-//   console.log(req.params.id,"/////////////////////////////")
-//   const user = await User.find({_id:req.params.id}).select("-password")
-  
-//   const posts = await Post.find({postedBy:req.params.id})  
-
-//   console.log(user,posts)
-//   res.json({user,posts})
-// })
 
 exports.addCategory = catchAsync(async(req,res,next) => {
  	
@@ -40,43 +22,7 @@ exports.addCategory = catchAsync(async(req,res,next) => {
     user
   })
 });
-// exports.unfollow = catchAsync(async(req,res,next) => {
- 	
-//     const user = await User.findByIdAndUpdate(req.user.id,{
-//         $pull:{following:req.body.id}
-//     },{
-//         new:true
-//     });
-//  	 await User.findByIdAndUpdate(req.body.id,{
-//         $pull:{follower:req.user.id}
-//     },{
-//         new:true
-//     });
 
-//   console.log(user,req.body.id)
-//   res.status(200).json({
-//     status:'success',
-//     user
-//   })
-// });
-// exports.getFollower = catchAsync(async(req,res,next) => {
-//   const user = await User.findByIdAndUpdate(req.user.id).populate({path:'follower',select:'name photo'});
-//   console.log(user)
-//   res.status(200).json({
-//     status:'success',
-//     user
-//   })
-
-// });
-// exports.getFollowing = catchAsync(async(req,res,next) => {
-//   const user = await User.findByIdAndUpdate(req.user.id).populate({path:'following',select:'name photo'});
-//   console.log(user)
-//   res.status(200).json({
-//     status:'success',
-//     user
-//   })
-
-// });
 
 exports.updateMe = catchAsync(async (req, res, next) => {
       let updatedUser;
